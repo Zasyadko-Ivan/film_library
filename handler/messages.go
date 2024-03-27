90ebeca2e919ec710cf8079a6406ac638bbcc147
Zasyadko-Ivan <yyeee31@gmail.com>
2024-03-27 16:04:50 +0300

    FR(handler): add a messages.go file containing the server responses

---
+++ handler/messages.go
@@ -0,0 +1,53 @@
+package handler
+
+const (
+	ServerError   = "Internal Server Error."
+	TokenNotFound = "Token not found."
+	TokenInvalid  = "Invalid token."
+	OnlyAdmin     = "Forbidden. Only admins are allowed to access this resource."
+)
+
+const (
+	MustPOST   = "The request method must be POST."
+	MustDELETE = "The request method must be DELETE."
+	MustPUT    = "The request method must be PUT."
+	MustGET    = "The request method must be GET."
+)
+
+const (
+	InvalidBirthday           = "Invalid birthday format. Please use YYYY-MM-DD."
+	InvalidReleaseDate        = "Invalid release_date format. Please use YYYY-MM-DD."
+	InvalidReplaceReleaseDate = "Invalid replace_release_date format. Please use YYYY-MM-DD."
+)
+
+const (
+	MissingRequiredFieldsActor = "Required fields (name, gender, birthday) are missing."
+	MissingRequiredFieldsFilm  = "Required fields (name, release_date) are missing."
+)
+
+const (
+	MissingRequiredArgNameActor = "The required 'name_actor' argument is missing."
+	MissingRequiredArgNameFilm  = "The required 'name_film' argument is missing."
+)
+
+const (
+	FilmCreated = "Film successfully create."
+	FilmDeleted = "Film successfully deleted."
+	FilmChange  = "Film successfully change."
+)
+
+const (
+	ErrFilmCreated    = "This film has already been added to the database."
+	ErrFilmNotCreated = "This film has not yet been added to the database."
+)
+
+const (
+	ActorCreated = "Actor successfully create."
+	ActorDeleted = "Actor successfully deleted."
+	ActorChange  = "Actor successfully change."
+)
+
+const (
+	ErrActorCreated    = "This actor has already been added to the database."
+	ErrActorNotCreated = "This actor has not yet been added to the database."
+)
